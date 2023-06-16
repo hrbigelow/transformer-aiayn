fix log normalizer overflow issues

aiayn/model.py
@@ -368,7 +368,9 @@ class Objective(hk.Module):
     @staticmethod
     def fused_kldiv_softmax(q, p_logits, axis):
         # compute D[q(x) || softmax(p_logits)] implicitly fusing the operations
-        log_normalizer = jnp.log(jnp.sum(jnp.exp(p_logits), axis))
+        z = jnp.max(p_logits, axis)
+        scaled_p_logits = p_logits - jnp.expand_dims(z, axis)
+        log_normalizer = z + jnp.log(jnp.sum(jnp.exp(scaled_p_logits), axis))
         q_entropy = - jnp.sum(jax.scipy.special.xlogy(q, q), axis)
         cross_entropy = - (jnp.sum(q * p_logits, axis) - log_normalizer)
         return cross_entropy - q_entropy