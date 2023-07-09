fix critical masking bug

aiayn/data.py
@@ -2,10 +2,17 @@ import os
 import sys
 import fire
 import numpy as np
+import jax.numpy as jnp
 from collections import Counter
 from transformers import GPT2TokenizerFast
 import tensorflow as tf
 import tensorflow_datasets as tfds
+
+def tokenize(query):
+    tokenizer = get_tokenizer()
+    bos_id = tokenizer.bos_token_id
+    eos_id = tokenizer.eos_token_id
+    return jnp.array([bos_id] + tokenizer(query)['input_ids'] + [eos_id])
 
 def de_tokenize(tokens):
     """
@@ -17,7 +24,7 @@ def de_tokenize(tokens):
     special_toks = (tok.bos_token_id, tok.eos_token_id, tok.pad_token_id)
     for i in range(tokens.shape[0]):
         toks = tokens[i]
-        toks = np.extract(~np.isin(toks, special_toks), toks) 
+        # toks = np.extract(~np.isin(toks, special_toks), toks) 
         toks = tok.convert_ids_to_tokens(toks)
         text = tok.convert_tokens_to_string(toks)
         ans.append(text)
@@ -45,11 +52,10 @@ def token_dataset(download_dir, split, dataset_name, nproc):
     # iterate once to populate cache
     return ds, ds_info
 
-def pad_dataset(token_ds, shuffle_size, max_sentence_length):
-    tokenizer = get_tokenizer()
-    bos = tf.constant([tokenizer.bos_token_id], tf.uint16)
-    eos = tf.constant([tokenizer.eos_token_id], tf.uint16)
-    pad_id = tokenizer.pad_token_id
+def pad_dataset(token_ds, token_info, shuffle_size, max_sentence_length):
+    bos = tf.constant([token_info['bos_token_id']], tf.uint16)
+    eos = tf.constant([token_info['eos_token_id']], tf.uint16)
+    pad_token_id = token_info['pad_token_id']
 
     def pad_tokens_fn(tok1, tok2):
         tok1 = tf.cast(tok1, tf.uint16)
@@ -58,8 +64,8 @@ def pad_dataset(token_ds, shuffle_size, max_sentence_length):
         sen_len2 = tf.shape(tok2)[0]
         l1 = max_sentence_length - sen_len1 - 2
         l2 = max_sentence_length - sen_len2 - 2
-        pad1 = tf.cast(tf.fill((l1,), pad_id), dtype=tf.uint16)
-        pad2 = tf.cast(tf.fill((l2,), pad_id), dtype=tf.uint16)
+        pad1 = tf.cast(tf.fill((l1,), pad_token_id), dtype=tf.uint16)
+        pad2 = tf.cast(tf.fill((l2,), pad_token_id), dtype=tf.uint16)
         tok1 = tf.concat(values=(bos, tok1, eos, pad1), axis=0)
         tok2 = tf.concat(values=(bos, tok2, eos, pad2), axis=0)
         return tok1, tok2, sen_len1, sen_len2 
@@ -85,10 +91,10 @@ def pipe_dataset(pad_ds, batch_size, swap_source_target):
     ds = tfds.as_numpy(ds)
     return ds
 
-def main_dataset(data_path, max_sentence_length, batch_size, swap_source_target):
+def main_dataset(data_path, token_info, max_sentence_length, batch_size, swap_source_target):
     token_ds = tf.data.Dataset.load(data_path)
     shuffle_size = len(token_ds)
-    pad_ds = pad_dataset(token_ds, shuffle_size, max_sentence_length)
+    pad_ds = pad_dataset(token_ds, token_info, shuffle_size, max_sentence_length)
     return pipe_dataset(pad_ds, batch_size, swap_source_target)
 
 def token_histo(dataset):

aiayn/model.py
@@ -51,7 +51,8 @@ class MultiHeadAttention(hk.Module):
         if self.masked:
             # query (d) can only attend to keys (c) at or before it 
             # mask[i,j] = -100 if i > j else 0
-            mask = jnp.tri(alogit.shape[2], k=-1) * -100.0
+            mask = jnp.tri(alogit.shape[3], k=-1) * -100.0
+            # print(f'MHA.__call__: {self.masked=}, {mask.shape=}, {alogit.shape=}')
             alogit += mask 
         att = jax.nn.softmax(alogit * self.scale_factor ** -1, axis=2)
         pre = jnp.einsum('bhcd,bhcv->bhdv', att, vval)
@@ -144,7 +145,7 @@ class InputEmbedding(hk.Module):
 class EncoderLayer(hk.Module):
     def __init__(self, hps, is_train, layer_num):
         super().__init__(name=f'layer{layer_num:02d}')
-        self.att = MultiHeadAttention(hps)
+        self.att = MultiHeadAttention(hps, masked=False)
         self.norm1 = DropoutAddAndNorm(hps, is_train)
         self.ff = PositionwiseFF(hps)
         self.norm2 = DropoutAddAndNorm(hps, is_train)
@@ -181,7 +182,7 @@ class DecoderLayer(hk.Module):
         super().__init__(name=f'layer{layer_num:02d}')
         self.mask_att = MultiHeadAttention(hps, masked=True)
         self.norm1 = DropoutAddAndNorm(hps, is_train)
-        self.att2 = MultiHeadAttention(hps)
+        self.att2 = MultiHeadAttention(hps, masked=False)
         self.norm2 = DropoutAddAndNorm(hps, is_train)
         self.ff = PositionwiseFF(hps)
         self.norm3 = DropoutAddAndNorm(hps, is_train)
@@ -218,6 +219,7 @@ class TeeSequential(hk.Module):
 class Decoder(hk.Module):
     def __init__(self, hps, is_train, T, embed_layer):
         super().__init__(name='dec')
+        self.is_train = is_train
         self.embed_layer = embed_layer 
         self.body = TeeSequential(*(DecoderLayer(hps, is_train, i) for i in range(hps.num_layers)))
         # self.linear_final = hk.Linear(T)
@@ -230,10 +232,14 @@ class Decoder(hk.Module):
         returns: bct
         """
         out = self.embed_layer(dec_in)
+        # print(f'Decoder.__call__: {enc_out.shape=}, {out.shape=}')
         out = self.body(enc_out, out)
         scaled_emb_mat = self.embed_layer.embed_mat() * self.scale_factor
         # out = self.linear_final(out)
-        out = jnp.einsum('bcm,tm -> bct', out, scaled_emb_mat)
+        if self.is_train:
+            out = jnp.einsum('bcm,tm -> bct', out, scaled_emb_mat)
+        else:
+            out = jnp.einsum('bm,tm -> bt', out[:,-1,:], scaled_emb_mat)
         # out = jax.nn.softmax(out, axis=2)
         return out
 
@@ -249,7 +255,7 @@ class Model(hk.Module):
         self.encoder = Encoder(hps, is_train, self.embed_layer)
         self.decoder = Decoder(hps, is_train, self.T, self.embed_layer)
 
-    def __call__(self, enc_input, dec_input):
+    def __call__(self, enc_input, dec_input, temperature):
         """
         enc_input: bc
         dec_input: bc
@@ -260,15 +266,19 @@ class Model(hk.Module):
             dec_output = self.decoder(enc_output, dec_input)
             return dec_output
 
+        B = enc_input.shape[0]
         C = self.hps.max_sentence_length
-
         enc_output = self.encoder(enc_input)
-        for p in range(C):
+        for p in range(1,C):
             rng_key = hk.next_rng_key()
             _, rng_key = jax.random.split(rng_key)
-            dec_output = self.decoder(enc_output, dec_input)
-            sample = jax.random.categorical(rng_key, dec_output[:, p], axis=1)
-            dec_input = dec_input.at[:,p].set(sample)
+            # print(f'{p=}, {enc_output.shape=}, {dec_input.shape=}')
+            dec_output = self.decoder(enc_output, dec_input) / temperature
+            sample = jax.random.categorical(rng_key, dec_output, axis=1)
+            sample = jnp.expand_dims(sample, axis=1) # shape = (B,1)
+            # print(f'{dec_input.shape=}, {dec_output.shape=}, {sample.shape=}')
+            dec_input = jnp.concatenate((dec_input, sample), axis=1)
+            # dec_input = dec_input.at[:,p].set(sample)
         return dec_input
 
     def total_params(self):

aiayn/sample.py
@@ -19,27 +19,30 @@ def main(query, hps_keys: str = 'arch,reg,data,sample', **hps_overrides):
         raise RuntimeError(f'Must provide the --resume_ckpt option')
     if hps.data_path is None:
         raise RuntimeError(f'Must provide the --data_path option')
+    try:
+        token_info = np.load(hps.token_info_file)
+    except:
+        raise RuntimeError(f'Could not load token info file {hps.token_info_file}')
 
-    token_info = data.load_token_info(hps.data_path) 
     mod = model.make_model(hps, False, token_info=token_info) 
     mngr = orbax.CheckpointManager(
         hps.ckpt_dir, orbax.Checkpointer(orbax.PyTreeCheckpointHandler()))
     params = mngr.restore(hps.resume_ckpt)
 
     rng_key = jax.random.PRNGKey(hps.random_seed)
-    tokenizer = data.get_tokenizer()
-    bos_id = tokenizer.bos_token_id
-    eos_id = tokenizer.eos_token_id
-    query_toks = jnp.array([bos_id] + tokenizer(query)['input_ids'] + [eos_id])
+    bos_id = token_info['bos_token_id']
+    query_toks = data.tokenize(query)
+
     print('Received query:')
     print(data.de_tokenize(np.expand_dims(query_toks, 0))[0])
 
     query_toks = jnp.repeat(jnp.expand_dims(query_toks, axis=0), hps.num_sample, axis=0)
-    dec_input = jnp.empty((hps.num_sample, hps.max_sentence_length), dtype=np.int32)
-    dec_input = query_toks.at[:,0].set(bos_id)
-    pred_toks = mod.apply(params, rng_key, query_toks, dec_input) 
+    dec_input = jnp.full((hps.num_sample, 1), bos_id, dtype=np.int32)
+    print(f'{query_toks.shape=}, {dec_input.shape=}')
+    pred_toks = mod.apply(params, rng_key, query_toks, dec_input, hps.temperature) 
     pred_sentences = data.de_tokenize(pred_toks)
     print('\n'.join(pred_sentences))
+    print(pred_toks)
 
 
 if __name__ == '__main__':