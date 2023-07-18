fixed critical bug in objective function in which masking was inverted.

aiayn/funcs.py
@@ -23,18 +23,36 @@ def entropy(p, axis):
     h_nat = jnp.sum(safe_xy(p, - jnp.log(p)), axis)
     return h_nat * log2e
 
+"""
+  x_max = jnp.max(x, axis, where=where, initial=initial, keepdims=True)
+  shifted = x - lax.stop_gradient(x_max)
+  shifted_logsumexp = jnp.log(
+      jnp.sum(jnp.exp(shifted), axis, where=where, keepdims=True))
+  result = shifted - shifted_logsumexp
+  if where is not None:
+    return jnp.where(where, result, -jnp.inf)
+  return result
+"""
+
+"""
+  z = jnp.max(p_logits, axis, where=where, initial=initial, keepdims=True)
+  scaled_p_logits = p_logits - lax.stop_gradient(z)
+  shifted_logsumexp = jnp.log(jnp.sum(jnp.exp(scaled_p_logits), axis))
+  log_normalizer = z + shifted_logsumexp
+  result = scaled_p_logits - shifted_logsumexp # original
+  result = p_logits - z - shifted_logsumexp    # rewrite
+  result = p_logits - log_normalizer           # rewrite
+  if where is not None:
+    return jnp.where(where, result, -jnp.inf)
+"""
+
 def fused_kldiv_softmax(q, p_logits, axis):
     # compute D[q(x) || softmax(p_logits)] implicitly fusing the operations
     # returns value in bits
+    p_logits = jax.nn.log_softmax(p_logits, axis)
     log2e = jnp.log2(jnp.exp(1.0))
-    z = jnp.max(p_logits, axis)
-    scaled_p_logits = p_logits - jnp.expand_dims(z, axis)
-    log_normalizer = z + jnp.log(jnp.sum(jnp.exp(scaled_p_logits), axis))
-    log_normalizer = jnp.expand_dims(log_normalizer, axis)
     log_q = jnp.log(q)
-    # q_entropy = - jnp.sum(jax.scipy.special.xlogy(q, q), axis)
-    # cross_entropy = - (jnp.sum(q * p_logits, axis) - log_normalizer)
-    kl_nats = jnp.sum(safe_xy(q, log_q - p_logits + log_normalizer), axis)
+    kl_nats = jnp.sum(safe_xy(q, log_q - p_logits), axis)
     kl = kl_nats * log2e
     # jax.debug.print("{}", kl) 
     return kl

aiayn/model.py
@@ -322,19 +322,27 @@ class Model(hk.Module):
 
             return dec_output
 
-        B, C = enc_input.shape
+        B, Ce = enc_input.shape
+        _, Cd = dec_input.shape
+        jnp.set_printoptions(precision=2, threshold=100000, edgeitems=100, linewidth=180)
+        # jax.debug.print('enc_mask: {}\n', enc_mask)
 
         enc_output = self.encoder(enc_embed, enc_mask)
         def sample_fn(i, dec_input): 
+            dec_embed = self.embed_layer(dec_input)
             rng_key = hk.next_rng_key()
             # print(f'{p=}, {enc_output.shape=}, {dec_input.shape=}')
             dec_output = self.decoder(enc_output, dec_embed, enc_mask, None) / temperature
             dec_step = jax.lax.dynamic_slice(dec_output, (0, i, 0), (B, 1, self.T))
+            # print('foo: ', dec_step.shape)
+            # jax.debug.print('dec_step: {}', dec_step)
             sample = jax.random.categorical(rng_key, dec_step, axis=2)
+            # jax.debug.print('sample: {}', sample)
             # print(f'{dec_step.shape=}, {sample.shape=}, {dec_input.shape=}')
             dec_input = dec_input.at[:,i+1].set(sample[:,0])
+            jax.debug.print('dec_input: {}', dec_input)
             return dec_input
-        dec_input = jax.lax.fori_loop(0, C-1, sample_fn, dec_input)
+        dec_input = jax.lax.fori_loop(0, Cd-1, sample_fn, dec_input)
         return dec_input
 
     def total_params(self):
@@ -379,12 +387,6 @@ class Objective(hk.Module):
         self.T = self.token_dist.shape[0]
         self.eps = smoothing_eps
 
-    @staticmethod
-    def masked_mean(values, mask):
-        # compute the mean of values at mask
-        masked_values = values * mask
-        return masked_values.sum() / mask.sum()
-
     def __call__(self, dec_input, dec_output_logits):
         """
         dec_input: bq
@@ -392,7 +394,7 @@ class Objective(hk.Module):
         """
         # bc
         targets = dec_input[:,1:]
-        targets_mask = jnp.equal(targets, self.mask_id).astype(jnp.float32)
+        targets_active = jnp.not_equal(targets, self.mask_id)
         # smoothing
         dist = jnp.expand_dims(self.token_dist, (0,1))
         targets = jax.nn.one_hot(targets, self.T, axis=2)
@@ -401,10 +403,9 @@ class Objective(hk.Module):
         dec_pred_logits = dec_output_logits[:,:-1,:]
         dec_pred = jax.nn.softmax(dec_pred_logits, axis=2)
         kldiv = funcs.fused_kldiv_softmax(targets, dec_pred_logits, 2)
-        # print(f'{targets.shape=}, {dec_pred_logits.shape=}, {kldiv.shape=}, {targets_mask.shape=}')
-        mean_kldiv = self.masked_mean(kldiv, targets_mask)
-        label_entropy = self.masked_mean(funcs.entropy(targets, axis=2), targets_mask)
-        model_entropy = self.masked_mean(funcs.entropy(dec_pred, axis=2), targets_mask)
+        mean_kldiv = kldiv.mean(where=targets_active)
+        label_entropy = funcs.entropy(targets, axis=2).mean(where=targets_active)
+        model_entropy = funcs.entropy(dec_pred, axis=2).mean(where=targets_active)
         # TODO: don't return model_entropy, it is wrong
         return mean_kldiv, label_entropy, model_entropy
 

tests/grad_tests.py
@@ -13,6 +13,10 @@ def make_mask(B, C):
     # Create a generic mask with the last half masked
     return jnp.indices((B,C))[1] * 2 // C
 
+def make_tokens(B, C, T):
+    rng_key = jax.random.PRNGKey(42)
+    return jax.random.categorical(rng_key, jnp.zeros(T), shape=(B,C))
+
 def print_grad(grad):
     jnp.set_printoptions(precision=2, threshold=100000, edgeitems=100, linewidth=180)
     print(grad, '\n\n')
@@ -96,19 +100,22 @@ def decoder_layer():
     _, _, _, dec_in_grad, _, _ = grads
     print_grad(dec_in_grad)
 
-
 def model_grads():
     H, M, K, V, F, L = tuple(arch[l] for l in 'HMKVFL')
-    B, Q, T = 2, 13, 20
-    inst_args = dropout_rate, arch, True  
-    out_shape = (B, Q, T)
-    input = jax.random.normal(rng_key, (B,T,M))
-    mask = jnp.array([ [0] * 8 + [1] * 5, [0] * 6 + [1] * 7 ])
+    B, Ce, Cd, T = 2, 13, 20, 30
+    pos_enc_factor = 0.01
+    inst_args = dropout_rate, pos_enc_factor, arch, True, T, T-1
+    enc_input = make_tokens(B, Ce, T)
+    dec_input = make_tokens(B, Cd, T)
     call_args = enc_input, dec_input
-
-    # grads = model.make_grads(model.Model, inst_args, 
+    out_grad = jnp.zeros((B,Cd,T), dtype=jnp.float32)
+    out_grad = out_grad.at[:,5,:].set(1.0)
+    grads = model.make_grads(model.Model, inst_args, out_grad, call_args)
+    _, _, enc_grad, dec_grad = grads
+    print('enc_grad:\n', enc_grad)
+    print('dec_grad:\n', dec_grad)
 
 if __name__ == '__main__':
     fire.Fire(dict(ff=ff, attn=attn, encoder=encoder, decoder=decoder,
-        decoder_layer=decoder_layer))
+        decoder_layer=decoder_layer, model_grads=model_grads))
 