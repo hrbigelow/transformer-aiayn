bf7727d4325737829998357121b1010957120d97
Henry Bigelow <hrbigelow@gmail.com>
2023-08-18 23:23:48 -0700

    using huggingface tokenizer again

--- aiayn/preprocess.py
+++ aiayn/preprocess.py
@@ -3,34 +3,68 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import fire
 import tensorflow as tf
 import tensorflow_datasets as tfds
-from transformers import PreTrainedTokenizerFast
+import numpy as np
+from tokenizers import Tokenizer
+from tokenizers.models import BPE
+from tokenizers.pre_tokenizers import Whitespace
+from tokenizers.trainers import BpeTrainer
 
-def token_dataset(download_dir, dataset_name, split, encode_fn, nproc):
+def train_tokenizer(dataset_name, split, data_dir, vocab_size, out_file):
+    builder = tfds.builder(dataset_name, data_dir=data_dir)
+    builder.download_and_prepare()
+    ds = builder.as_dataset(split=split, shuffle_files=False)
+    num_elems = len(ds) * 2
+    # ds = ds.map(map_fn)
+    ds = ds.batch(1000)
+
+    def convert(ds):
+        it = ds.as_numpy_iterator()
+        decode = np.vectorize(lambda x: x.decode())
+        while True:
+            item = next(it, None)
+            if item is None:
+                return
+            one, two = item.values()
+            yield decode(one)
+            yield decode(two)
+
+    tokenizer = Tokenizer(BPE(unk_token='[UNK]'))
+
+    trainer = BpeTrainer(
+            vocab_size=vocab_size,
+            special_tokens=['[UNK]', '[PAD]', '[EOS]', '[BOS]'])
+
+    tokenizer.pre_tokenizer = Whitespace()
+    tokenizer.train_from_iterator(convert(ds), trainer, num_elems)
+    tokenizer.save(out_file)
+
+
+def token_dataset(download_dir, dataset_name, split, tokenizer_file, nproc):
     """
     """
+    tokenizer = Tokenizer.from_file(tokenizer_file)
     builder = tfds.builder(dataset_name, data_dir=download_dir)
     # the download_dir argument of download_and_prepare seems to be ignored in favor
     # of tfds.builder(data_dir=...)
     builder.download_and_prepare()
     ds = builder.as_dataset(split=split, shuffle_files=True)
-    ds_info = builder.info
+    num_elem = len(ds)
+    ds = ds.batch(1000)
 
-    def tokenize_fn(item):
-        def _py_fn(one, two):
-            one = encode_fn(one.numpy().decode())
-            two = encode_fn(two.numpy().decode())
-            return tf.constant(one, dtype=tf.uint16), tf.constant(two, dtype=tf.uint16)
-        return tf.py_function(_py_fn, inp=item.values(), Tout=[tf.uint16, tf.uint16])
+    def gen(ds):
+        it = ds.as_numpy_iterator() 
+        unicode_decode = np.vectorize(lambda x: x.decode())
+        while True:
+            item = next(it, None)
+            if item is None:
+                return
+            one, two = item.values()
+            one = tokenizer.encode_batch(unicode_decode(one))
+            two = tokenizer.encode_batch(unicode_decode(two))
+            yield from [(a.ids, b.ids) for a, b in zip(one, two)]
+    return gen(ds), num_elem
 
-    ds = ds.map(tokenize_fn, num_parallel_calls=nproc, deterministic=False)
-    # No need for prefetching or caching since this dataset will be
-    # compiled into tfrecords
-    # ds.prefetch(ds_info.splits[split].num_examples)
-    # ds = ds.cache(f'{download_dir}/{dataset_name}-cache')
-    # iterate once to populate cache
-    return ds, ds_info
-
-def write_records(ds, path_template, num_shards, shards=None):
+def write_records(data_gen, num_elem, path_template, num_shards, shards=None):
     """
     Transform all records in ds, writing them to `num_shards` separate
     `path_template` files.
@@ -46,11 +80,11 @@ def write_records(ds, path_template, num_shards, shards=None):
             output_buffer_size=10000)
 
     shards = range(num_shards)
-    chunk_size = len(ds) // num_shards
+    chunk_size = num_elem // num_shards
     begs = [chunk_size * i for i in range(num_shards)]
 
     chunk = -1
-    for i, (t1, t2) in enumerate(iter(ds)):
+    for i, (t1, t2) in enumerate(data_gen):
         if chunk != i // chunk_size:
             chunk = i // chunk_size
             record_path = path_template.format(chunk)
@@ -72,32 +106,17 @@ def write_records(ds, path_template, num_shards, shards=None):
         record_bytes = example.SerializeToString()
         file_writer.write(record_bytes)
 
-def main(download_dir, dataset_name, split, vocab_dir, nproc, num_shards,
+def main(download_dir, dataset_name, split, tokenizer_file, nproc, num_shards,
         out_template, shards=None):
     """
     Write a tfrecord dataset to out_template (must contain '{}') 
     """
-    from tensor2tensor import problems
-    ende_problem = problems.problem('translate_ende_wmt32k')
-    encoders = ende_problem.feature_encoders(vocab_dir)
-    encode_fn = encoders['inputs'].encode
-
     # tokenizer = PreTrainedTokenizerFast(tokenizer_file=tokenizer_file)
     print('Preparing dataset')
-    ds, _ = token_dataset(download_dir, dataset_name, split, encode_fn, nproc)
+    data_gen, num_elem = token_dataset(download_dir, dataset_name, split,
+            tokenizer_file, nproc)
     print('Writing tfrecords')
-    write_records(ds, out_template, num_shards, shards) 
-
-def encoder_stats(vocab_dir):
-    from tensor2tensor import problems
-    ende_problem = problems.problem('translate_ende_wmt32k')
-    encoders = ende_problem.feature_encoders(vocab_dir)
-    # in this case the input and target encoders are the same
-    encoder = encoders['inputs']
-    print(f'n_vocab: {encoder.vocab_size}')
-    print(f'token 0: {encoder.decode([0])}')
-    print(f'token 1: {encoder.decode([1])}')
-
+    write_records(data_gen, num_elem, out_template, num_shards, shards) 
 
 if __name__ == '__main__':
     cmds=dict(make=main, encoder_stats=encoder_stats)
