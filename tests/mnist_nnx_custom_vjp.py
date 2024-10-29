6815d714163e588381491294842535baf9ad5734
Henry Bigelow <hrbigelow@gmail.com>
2024-10-28 23:01:31 -0700

    working with and without imm_grad

--- tests/mnist_nnx_custom_vjp.py
+++ tests/mnist_nnx_custom_vjp.py
@@ -1,8 +1,10 @@
 import optax
+import fire
 import tensorflow_datasets as tfds  # TFDS to download MNIST.
 from flax import nnx
 from flax.nnx import extract, graph
 from flax.nnx.transforms import general
+from flax.nnx.training import optimizer as optim
 import jax
 import typing as tp
 import functools
@@ -10,34 +12,23 @@ from functools import partial
 import tensorflow as tf  # TensorFlow / `tf.data` operations.
 from aiayn.mod_optimizer import ModuleOptimizer
 
-tf.random.set_seed(0)  # Set the random seed for reproducibility.
+def make_dataset(split='train', seed=0, batch_size=32, total_steps=10000):
+  ds: tf.data.Dataset = tfds.load('mnist', split=split)
+  tf.random.set_seed(seed)  # Set the random seed for reproducibility.
+  ds = ds.map(
+    lambda sample: {
+      'image': tf.cast(sample['image'], tf.float32) / 255,
+      'label': sample['label'],
+    }
+  )
+  if split == 'train':
+    ds = ds.repeat().shuffle(1024)
 
-train_steps = 12000
-eval_every = 100
-batch_size = 32
-
-train_ds: tf.data.Dataset = tfds.load('mnist', split='train')
-test_ds: tf.data.Dataset = tfds.load('mnist', split='test')
-
-train_ds = train_ds.map(
-  lambda sample: {
-    'image': tf.cast(sample['image'], tf.float32) / 255,
-    'label': sample['label'],
-  }
-)  # normalize train set
-test_ds = test_ds.map(
-  lambda sample: {
-    'image': tf.cast(sample['image'], tf.float32) / 255,
-    'label': sample['label'],
-  }
-)  # Normalize the test set.
-
-# Create a shuffled dataset by allocating a buffer size of 1024 to randomly draw elements from.
-train_ds = train_ds.repeat().shuffle(1024)
-# Group into batches of `batch_size` and skip incomplete batches, prefetch the next sample to improve latency.
-train_ds = train_ds.batch(batch_size, drop_remainder=True).take(train_steps).prefetch(1)
-# Group into batches of `batch_size` and skip incomplete batches, prefetch the next sample to improve latency.
-test_ds = test_ds.batch(batch_size, drop_remainder=True).prefetch(1)
+  ds = ds.batch(batch_size, drop_remainder=True)
+  if split == 'train': 
+    ds = ds.take(total_steps)
+  ds = ds.prefetch(1)
+  return ds
 
 F = tp.TypeVar('F', bound=tp.Callable[..., tp.Any])
 
@@ -52,60 +43,81 @@ def nnx_vjp(f, ins, out_g):
     return ins_g.state
   return ins_g
 
-DO_CUSTOM_VJP = True
-DO_IMM_GRAD = True
-# DO_IMM_GRAD = True
-
 class Reactive(nnx.Module):
-  def __init__(self, module_cls: tp.Type, *args, **kwargs):
+  def __init__(self, module_cls: 
+               tp.Type, 
+               tx: optax.GradientTransformation,
+               do_imm_grad,
+               *args, **kwargs):
     self.mod = module_cls(*args, **kwargs)
-
-  def post_init(self, opt: ModuleOptimizer):
-    self.opt = opt
-    if DO_CUSTOM_VJP:
-      module_path = opt.find_node_path(self.mod)
-      self.imm_fn = make_custom_vjp(lambda mod, x: mod(x), self.opt, module_path)
-    else:
-      self.imm_fn = lambda mod, x: mod(x)
+    self.opt = MyOptimizer(self.mod, tx)
+    self.imm_fn = make_custom_vjp(do_imm_grad)
 
   def __call__(self, x: jax.Array):
-    return self.imm_fn(self.mod, x)
+    return self.imm_fn(self.opt, x)
 
-def make_custom_vjp(fn, mod_optimizer: ModuleOptimizer, module_path: tp.Tuple[str, ...]):
+def make_custom_vjp(do_imm_grad=True):
   """
   Return a function which is enabled for immediately updated gradient 
   """
-  @nnx.custom_vjp
-  def primal(mod, x):
-    return fn(mod, x)
+  def fn(opt, x):
+    return opt.model(x)
 
-  def fn_fwd(mod, x):
-    return fn(mod, x), (mod, x)
+  if not do_imm_grad:
+    return fn
+
+  @nnx.custom_vjp
+  def primal(opt, x):
+    return fn(opt, x)
+
+  def fn_fwd(opt, x):
+    return fn(opt, x), (opt, x)
 
   def fn_bwd(res, g):
     ins_g, out_g = g
-    m, x = res
-    param_g = nnx_vjp(lambda mod: fn(mod, x), m, out_g)
+    opt, x = res
+    opt_g = nnx_vjp(lambda opt_arg: opt_arg.model(x), opt, out_g)
     # param_g = jax.lax.pmean(param_g, 'dev') # ? each gradient on a different device
-    if DO_IMM_GRAD:
-      mod_optimizer.update(m, module_path, param_g) # per-module updates 
-    data_g = nnx_vjp(lambda data: fn(m, data), x, out_g)
-    # print(f'{param_g = }, {data_g = }')
-    return (param_g, data_g) # TODO: param_g unnecessary
+    new_params = opt.get_updated(opt_g['model'])
+    graphdef = nnx.graphdef(opt.model)
+    new_model = nnx.merge(graphdef, new_params)
+    data_g = nnx_vjp(lambda x_arg: new_model(x_arg), x, out_g)
+    # print(f'in do_imm_grad')
+    # opt.update(opt_g['model']) 
+    # print(f'otherwise')
+    return (opt_g, data_g) # TODO: param_g unnecessary
 
   primal.defvjp(fn_fwd, fn_bwd)
   return primal
 
+class MyOptimizer(nnx.Optimizer):
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+
+  def get_updated(self, grads):
+    """
+    Get the updated params without updating any state
+    """
+    params = nnx.state(self.model, self.wrt)
+    opt_state = optim._opt_state_variables_to_state(self.opt_state)
+
+    updates, new_opt_state = self.tx.update(grads, opt_state, params)
+    new_params = optax.apply_updates(params, updates)
+    assert isinstance(new_params, nnx.State)
+    return new_params
 
 class CNN(nnx.Module):
   """A simple CNN model."""
 
-  def __init__(self, *, rngs: nnx.Rngs):
-    self.conv1 = Reactive(nnx.Conv, 1, 32, kernel_size=(3, 3), rngs=rngs)
-    self.conv2 = Reactive(nnx.Conv, 32, 64, kernel_size=(3, 3), rngs=rngs)
+  def __init__(self, 
+               tx: optax.GradientTransformation, 
+               do_imm_grad: bool,
+               rngs: nnx.Rngs):
+    self.conv1 = Reactive(nnx.Conv, tx, do_imm_grad, 1, 32, kernel_size=(3, 3), rngs=rngs)
+    self.conv2 = Reactive(nnx.Conv, tx, do_imm_grad, 32, 64, kernel_size=(3, 3), rngs=rngs)
     self.avg_pool = partial(nnx.avg_pool, window_shape=(2, 2), strides=(2, 2))
-    self.linear1 = Reactive(nnx.Linear, 3136, 256, rngs=rngs)
-    self.linear2 = Reactive(nnx.Linear, 256, 10, rngs=rngs)
+    self.linear1 = Reactive(nnx.Linear, tx, do_imm_grad, 3136, 256, rngs=rngs)
+    self.linear2 = Reactive(nnx.Linear, tx, do_imm_grad, 256, 10, rngs=rngs)
 
   def __call__(self, x):
     x = self.avg_pool(nnx.relu(self.conv1(x)))
@@ -115,97 +127,90 @@ class CNN(nnx.Module):
     x = self.linear2(x)
     return x
 
-# Instantiate the model.
-model = CNN(rngs=nnx.Rngs(0))
-# print('\n'.join(str(p) + ' ' + m.__class__.__name__ for p, m in model.iter_modules()))
 
-learning_rate = 0.005
-momentum = 0.9
-tx = optax.adamw(learning_rate, momentum)
-optimizer = nnx.Optimizer(model, tx)
-mod_optimizer = ModuleOptimizer(tx)  
-mod_optimizer.post_init(model)
+def main(learning_rate=0.0005, 
+         momentum=0.9, 
+         eval_every=100, 
+         train_steps=15000,
+         do_eval=False,
+         do_imm_grad=True):
+  tx = optax.adamw(learning_rate, momentum)
+  model = CNN(tx, do_imm_grad, rngs=nnx.Rngs(0))
+  optimizer = nnx.Optimizer(model, tx)
 
-# post-init
-for _, mod in model.iter_modules():
-    if hasattr(mod, 'post_init'):
-        mod.post_init(mod_optimizer)
+  metrics = nnx.MultiMetric(
+    accuracy=nnx.metrics.Accuracy(),
+    loss=nnx.metrics.Average('loss'),
+  )
 
+  def loss_fn(model: CNN, batch):
+    logits = model(batch['image'])
+    loss = optax.softmax_cross_entropy_with_integer_labels(
+      logits=logits, labels=batch['label']
+    ).mean()
+    return loss, logits
 
-for p, m in model.iter_modules():
-    p2 = mod_optimizer.find_node_path(m)
-    assert p == p2, f'{p = } != {p2 = }'
-
-metrics = nnx.MultiMetric(
-  accuracy=nnx.metrics.Accuracy(),
-  loss=nnx.metrics.Average('loss'),
-)
-
-def loss_fn(model: CNN, batch):
-  logits = model(batch['image'])
-  loss = optax.softmax_cross_entropy_with_integer_labels(
-    logits=logits, labels=batch['label']
-  ).mean()
-  return loss, logits
-
-@nnx.jit
-def train_step(model: CNN, optimizer: nnx.Optimizer, metrics: nnx.MultiMetric, batch):
-  """Train for a single step."""
-  grad_fn = nnx.value_and_grad(loss_fn, has_aux=True)
-  (loss, logits), grads = grad_fn(model, batch)
-  metrics.update(loss=loss, logits=logits, labels=batch['label'])  # In-place updates.
-  if not DO_IMM_GRAD:
+  @nnx.jit
+  def train_step(model: CNN, optimizer: nnx.Optimizer, metrics: nnx.MultiMetric, batch):
+    """Train for a single step."""
+    grad_fn = nnx.value_and_grad(loss_fn, has_aux=True)
+    (loss, logits), grads = grad_fn(model, batch)
+    metrics.update(loss=loss, logits=logits, labels=batch['label'])  # In-place updates.
     optimizer.update(grads)  # In-place updates.
 
-@nnx.jit
-def eval_step(model: CNN, metrics: nnx.MultiMetric, batch):
-  loss, logits = loss_fn(model, batch)
-  metrics.update(loss=loss, logits=logits, labels=batch['label'])  # In-place updates.
+  @nnx.jit
+  def eval_step(model: CNN, metrics: nnx.MultiMetric, batch):
+    loss, logits = loss_fn(model, batch)
+    metrics.update(loss=loss, logits=logits, labels=batch['label'])  # In-place updates.
 
 
-metrics_history = {
-  'train_loss': [],
-  'train_accuracy': [],
-  'test_loss': [],
-  'test_accuracy': [],
-}
+  metrics_history = {
+    'train_loss': [],
+    'train_accuracy': [],
+    'test_loss': [],
+    'test_accuracy': [],
+  }
 
-print(f'{DO_IMM_GRAD=}, {DO_CUSTOM_VJP=}')
+  batch_size = 32
+  train_ds = make_dataset('train', 0, batch_size, train_steps)
+  test_ds = make_dataset('test', 10, batch_size, 0)
 
-for step, batch in enumerate(train_ds.as_numpy_iterator()):
-  # Run the optimization for one step and make a stateful update to the following:
-  # - The train state's model parameters
-  # - The optimizer state
-  # - The training loss and accuracy batch metrics
-  train_step(model, optimizer, metrics, batch)
+  for step, batch in enumerate(train_ds.as_numpy_iterator()):
+    # Run the optimization for one step and make a stateful update to the following:
+    # - The train state's model parameters
+    # - The optimizer state
+    # - The training loss and accuracy batch metrics
+    train_step(model, optimizer, metrics, batch)
+    # print(f'{model.conv1.opt.step = }, {optimizer.step = }')
 
-  if step > 0 and (step % eval_every == 0 or step == train_steps - 1):  # One training epoch has passed.
-    # Log the training metrics.
-    for metric, value in metrics.compute().items():  # Compute the metrics.
-      metrics_history[f'train_{metric}'].append(value)  # Record the metrics.
-    metrics.reset()  # Reset the metrics for the test set.
+    if step > 0 and (step % eval_every == 0 or step == train_steps - 1):  # One training epoch has passed.
+      # Log the training metrics.
+      for metric, value in metrics.compute().items():  # Compute the metrics.
+        metrics_history[f'train_{metric}'].append(value)  # Record the metrics.
+      metrics.reset()  # Reset the metrics for the test set.
 
-    # Compute the metrics on the test set after each training epoch.
-    for test_batch in test_ds.as_numpy_iterator():
-      eval_step(model, metrics, test_batch)
+      # Compute the metrics on the test set after each training epoch.
+      if do_eval:
+        for test_batch in test_ds.as_numpy_iterator():
+          eval_step(model, metrics, test_batch)
 
-    # Log the test metrics.
-    for metric, value in metrics.compute().items():
-      metrics_history[f'test_{metric}'].append(value)
-    metrics.reset()  # Reset the metrics for the next training epoch.
+        # Log the test metrics.
+        for metric, value in metrics.compute().items():
+          metrics_history[f'test_{metric}'].append(value)
+        metrics.reset()  # Reset the metrics for the next training epoch.
 
-    print(
-      f"[train] step: {step}, "
-      f"loss: {metrics_history['train_loss'][-1]:5.3f}, "
-      f"accuracy: {metrics_history['train_accuracy'][-1] * 100:5.3f}"
-    )
-    print(
-      f"[test ] step: {step}, "
-      f"loss: {metrics_history['test_loss'][-1]:5.3f}, "
-      f"accuracy: {metrics_history['test_accuracy'][-1] * 100:5.3f}"
-    )
+      print(
+          f"imm_grad: {do_imm_grad} [train] step: {step}, "
+          f"loss: {metrics_history['train_loss'][-1]:5.3f}, "
+          f"accuracy: {metrics_history['train_accuracy'][-1] * 100:5.3f}"
+      )
+      if do_eval:
+        print(
+            f"imm_grad: {do_imm_grad} [test ] step: {step}, "
+            f"loss: {metrics_history['test_loss'][-1]:5.3f}, "
+            f"accuracy: {metrics_history['test_accuracy'][-1] * 100:5.3f}"
+        )
 
-  
-
-
+if __name__ == '__main__':
+  fire.Fire(main)
 
